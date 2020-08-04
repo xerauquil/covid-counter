635d6414f25dbad6fcf86e9d08265a6908e8f1e6
Alexander Hristov <xerauquil@gmail.com>
2020-08-05 00:45:07 +0300

    gh pages #ilostcount Change BrowserRouter to HashRouter because stupid github pages doesn't support fallback to index on 404 :/ Gonna stick to ugly URLs for now.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import theme from './theme';
