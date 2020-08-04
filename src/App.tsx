1e18de05f6e2acc8d9bf394bc599188742eb2ab6
Alexander Hristov <xerauquil@gmail.com>
2020-08-05 00:38:15 +0300

    gh pages #3

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <main className={mainClassname}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route path='/' exact>
               <Global />
